cfa9f41a5a63ddcc420a5587671acbfa64a8c947
m-rosinsky <michael.rosinsky@uconn.edu>
2023-10-08 01:04:55 +0000

    Added initial CMake configuration

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*!
+ * @file src/main.cpp
+ *
+ * @brief This file contains the entry point for the program.
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+}
+
+/***   end of file   ***/
