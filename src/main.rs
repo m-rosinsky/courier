c81f4a003437ae2f6474e4996d434d0a05584d37
m-rosinsky <michael.rosinsky@uconn.edu>
2023-10-20 01:19:52 +0000

    Added clap for command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+/*!
+ * @file src/main.rs
+ *
+ * @brief This file containts the main function of the program.
+ */
+
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+    // Configure clap for command line args.
+    let matches = clap::App::new("Courier")
+        .version("0.1.0")
+        .author("Mike Rosinsky")
+        .about("A network-focused interpreted language")
+        .arg(clap::Arg::with_name("in_file")
+            .help("The input file to execute")
+            .value_name("FILE")
+            .required(true))
+        .get_matches();
+
+    if let Some(input_file) = matches.value_of("in_file") {
+        println!("Input file: {}", input_file);
+    }
 }
+
+/***   end of file   ***/
